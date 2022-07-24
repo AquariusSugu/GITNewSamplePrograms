6adc087b384c37b3304dfcbb49b0137194838c17
AquariusSugu <suganya733senthil2gmail.com>
2022-07-24 12:19:42 +0530

    Commited

---
+++ src/com/gitnewsample/GitSampleJAVAPgms.java
@@ -0,0 +1,42 @@
+package com.gitnewsample;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class GitSampleJAVAPgms {
+
+	
+	  public static void main(String[] args) {
+	      //System.out.println("Hello, World!"); 
+//	      String s="Suganya";
+//	      String output="";
+//	      for(int i=s.length()-1;i>=0;i--){
+//	          char c=s.charAt(i);
+//	          output=output+c;
+//	      }
+//	      output=output+"";
+//	    System.out.println(output);
+//	   
+	    
+	    String s1="java";
+        Map<Character,Integer> map=new LinkedHashMap<>();
+        for(int i=0; i<s1.length(); i++){
+            char c=s1.charAt(i);
+            if(map.containsKey(c)){
+                Integer in=map.get(c);
+                map.put(c,in+1);
+            }
+            else{
+                map.put(c,1);
+            }
+            System.out.println(map);
+        }
+        
+        
+        
+	    
+	    
+	    
+	    }   
+	  
+}
